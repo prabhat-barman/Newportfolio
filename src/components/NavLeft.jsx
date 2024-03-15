77fb6a335683ff0fb1c434e34d42c0edc7af12cd
Prabhat Barman <PBarman@bpl.netlink.com>
2024-03-15 16:10:35 +0530

    check

--- src/components/NavLeft.jsx
+++ src/components/NavLeft.jsx
@@ -11,7 +11,7 @@ import { useState } from "react";
 
 const NavLeft = (formData) => {
   const [first, setFirst] = useState("home");
-  console.log(formData);
+  console.log("ss", formData);
 
   let componentToRender;
 
@@ -41,7 +41,9 @@ const NavLeft = (formData) => {
   return (
     <>
       <div className='nav bg-black h-screen w-44 h-full font-["Montserrat"] text-white flex flex-col gap-16 items-start'>
-        <h1 className="font-bold text-xl ml-7 mt-7">{formData?.firstName}.</h1>
+        <h1 className="font-bold text-white text-xl ml-7 mt-7">
+          {formData?.formData?.firstName}.
+        </h1>
         <div className="links flex flex-col gap-3">
           <button
             onClick={() => setFirst("home")}
